435fc710729fd4c467f6ce6a39e79ad8e25505ac
mozzan <linch0520@gmail.com>
2015-03-23 16:37:31 +0800

    Atoi

--- src/com/mozzan/leetcode/StringToInteger_atoi.java
+++ src/com/mozzan/leetcode/StringToInteger_atoi.java
@@ -5,21 +5,61 @@ import java.lang.String;
 public class StringToInteger_atoi {
 
 	public static void main(String[] args) {
-		System.out.println(new StringToInteger_atoi().atoi("-3.1335"));
+		System.out.println(new StringToInteger_atoi().atoi("-222"));
 		//BigDecimal b1 = new BigDecimal("1.2");
 		//BigDecimal b2 = new BigDecimal("1.20");
 		//System.out.println(b1.intValue());
 	}
 
 	public int atoi(String str) {
-		float anwser =  Float.parseFloat(str);
-		if(anwser < -2147483648) {
-			return 0;
-		} else if(anwser > 2147483647) {
-			return 0;
+		int len = str.length();
+		char[] number = str.toCharArray();
+		boolean isStart = false;
+		boolean isNeg = false;
+		int index = 0;
+		long total = 0;
+
+		while(index < len) {
+			if(!isStart) {
+				if(number[index] == ' ') {
+					isStart = false;
+				} else if(number[index] == '-') {
+					isNeg = true;
+					isStart = true;
+					index++;
+					continue;
+				} else if(number[index] == '+') {
+					isNeg = false;
+					isStart = true;
+					index++;
+					continue;
+				} else if(number[index] >= '0' && number[index] <= '9') {
+					isStart = true;
+				} else {
+					return 0;
+				}
+			}
+
+			if(!isStart) {
+				index++;
+				continue;
+			}
+			if(number[index] >= '0' && number[index] <= '9') {
+				total = total * 10 + (isNeg ? 0 - (number[index] - '0') : number[index] - '0');
+				if(isNeg && total > 0) {
+					total *= -1;
+				}
+				if(total < -2147483648) {
+					return -2147483648;
+				} else if(total > 2147483647) {
+					return 2147483647;
+				}
+			} else {
+				break;
+			}
+			index++;
 		}
 
-		return (int)anwser;
+		return (int)total;
 	}
-
 }
